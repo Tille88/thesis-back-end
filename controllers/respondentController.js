491a7a8af1c9d2cfdaa7c5cee87b484f7f5322f1
Jonas Tillman <tillman.jonas@gmail.com>
2021-01-04 12:38:47 +0800

    Colprog to circel not to remember alpha-mapping

--- controllers/respondentController.js
+++ controllers/respondentController.js
@@ -4,6 +4,7 @@ const uuid = require('uuid');
 const utils = require('../utils/utils');
 
 
+// TODO-maybe: exchange for random lookups of progressions from DB?
 const IMAGE_COUNT = 10;
 
 let imgIdxs = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021"];
@@ -11,15 +12,21 @@ let cols = ["red", "blue", "green"];
 let legendTypes = ["annotated", "checkered", "clustered", "headline", "sampled"];
 
 function generateRandomSelection(){
-  legendProg = [];
+  let legendProg = [];
   while(legendProg.length<IMAGE_COUNT){
     utils.shuffleArray(legendTypes);
     legendProg = legendProg.concat(legendTypes);
   }
 
+  let colProg = [];
+  utils.shuffleArray(cols);
+  while(colProg.length<IMAGE_COUNT){
+    colProg = colProg.concat(cols);
+  }
+
   utils.shuffleArray(imgIdxs);
   return imgIdxs.slice(0, IMAGE_COUNT).map((num, idx) => {
-    return `${process.env.IMAGEPATH}${num}-${cols[utils.randomInt(0,2)]}-${legendProg[idx]}-legend-merge.png`;
+    return `${process.env.IMAGEPATH}${num}-${colProg[idx]}-${legendProg[idx]}-legend-merge.png`;
   });
 }
 
